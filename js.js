1685362171e50ee6bda950b6c03184d84f3dd112
Richard Washington <richard.washington@taxassist.co.uk>
2022-08-28 10:57:48 +0100

    :sparkles: Add some basic tests and refactor into functions

--- js.js
+++ js.js
@@ -4,6 +4,47 @@ function getComputerChoice() {
     return moves[Math.floor(Math.random()  * 3)];
 }
 
+function getComputerChoiceTest() {
+    let choice = getComputerChoice();
+    if(choice === 'rock' || choice === 'paper' || choice === 'scissors') {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function playRound(playerChoice, compChoice) {
+    if(playerChoice === 'rock' && compChoice === 'scissors') {
+        return 'You win';
+    } else if(playerChoice === 'rock' && compChoice === 'paper') {
+        return 'You lose';
+    } else if(playerChoice === 'paper' && compChoice === 'rock') {
+        return 'You win!';
+    } else if(playerChoice === 'paper' && compChoice === 'scissors') {
+        return 'You lose';
+    } else if(playerChoice === 'scissors' && compChoice === 'paper') {
+        return 'You win!';
+    } else if(playerChoice === 'scissors' && compChoice === 'rock') {
+        return 'You lose';
+    } else if (playerChoice === compChoice) {
+        return 'Draw';
+    }
+}
+
+function playRoundTest() {
+    let result1 = playRound('Paper', 'Rock');
+    let result2 = playRound('Paper', 'Scissors');
+    let result3 = playRound('Paper', 'Paper');
+
+    if(result1 === 'You win!' || result2 === 'You lose' || result3 === 'Draw') {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+console.log(playRoundTest());
+console.log(getComputerChoiceTest());
 
 let compChoice = getComputerChoice();
 let playerChoice = prompt();
@@ -11,18 +52,7 @@ let playerChoice = prompt();
 console.log('The computer chose ' + compChoice);
 console.log('The player chose ' + playerChoice);
 
-if(playerChoice === 'rock' && compChoice === 'scissors') {
-    console.log('You win')
-} else if(playerChoice === 'rock' && compChoice === 'paper') {
-    console.log('You lose')
-} else if(playerChoice === 'paper' && compChoice === 'rock') {
-    console.log('You win!')
-} else if(playerChoice === 'paper' && compChoice === 'scissors') {
-    console.log('You lose');
-} else if(playerChoice === 'scissors' && compChoice === 'paper') {
-    console.log('You win!');
-} else if(playerChoice === 'scissors' && compChoice === 'rock') {
-    console.log('You lose');
-} else if (playerChoice === compChoice) {
-    console.log('Draw');
-}
+console.log(playRound(playerChoice, compChoice));
+
+
+
